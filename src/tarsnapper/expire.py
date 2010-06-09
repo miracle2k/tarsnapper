4a4c536a79fe5a39f6f26718aa70c0c44fe47bf5
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-06-09 06:45:01 +0200

    Basic initial that can expire/delete backups.

---
+++ src/tarsnapper/expire.py
@@ -0,0 +1,77 @@
+from datetime import datetime, timedelta
+
+
+__all__ = ('expire',)
+
+
+def timedelta_div(td1, td2):
+    """http://stackoverflow.com/questions/865618/how-can-i-perform-divison-on-a-datetime-timedelta-in-python
+    """
+    us1 = td1.microseconds + 1000000 * (td1.seconds + 86400 * td1.days)
+    us2 = td2.microseconds + 1000000 * (td2.seconds + 86400 * td2.days)
+    return float(us1) / us2
+
+
+def expire(backups, deltas):
+    """Given a dict of backup name => backup timestamp pairs in
+    ``backups``, and a list of ``timedelta`` objects in ``deltas`` defining
+    the generations, will decide which of the backups can be deleted using
+    a  grandfather-father-son backup strategy.
+
+    This approach insists on keeping a minimum number of backups in each
+    generation, regardless of their timestamp. The number of backups in a
+    generation is implicitly defined by the deltas. For example, if the
+    first delta is 1 hour, and the second delta is 1 day, then we will
+    always try to keep 24 backups in the first generation, even if they
+    stretch over, say, the timespan of a year. So for example, if you put
+    your backups on hold for a year, and then pick up again, the old backups
+    will not be immediately deleted, but only after a sufficient number of
+    new backups are available.
+
+    This is what I wanted to accomplish:
+
+      * Do not require backup names to include information on which
+        generation a backup belongs to, like for example
+        ``tarsnap-generations`` does.
+
+      * Do not make any assumptions about when the backup jobs have
+        actually run (this expire function is completely separate from the
+        actual backup creation), and when in doubt opt to keep backups
+        rather than deleting them.
+
+    Returned is a list of backup names.
+    """
+
+    # Deal with some special cases
+    assert len(deltas) > 2, "At least two deltas are required"
+    if not backups:
+        return []
+
+    # First, sort the backups with most recent one first
+    backups = [(name, time) for name, time in backups.items()]
+    backups.sort(cmp=lambda x, y: cmp(x[1], y[1]))
+    old_backups = backups[:]
+
+    # Also make sure that we have the deltas in ascending order
+    deltas = list(deltas[:])
+    deltas.sort()
+
+    latest_backup = backups.pop()
+    dt_incr = latest_backup[1]
+    to_keep = [latest_backup[0]]
+    while len(deltas) > 1:   # the last delta is the final barrier
+        current_delta = deltas.pop(0)
+        next_delta = deltas[0]
+        num_backups_in_generation = timedelta_div(next_delta, current_delta)
+
+        # For the number of backups needed, use the first backup that is
+        # not younger than the requested limit.
+        while num_backups_in_generation > 0 and backups:
+            name, backup_time = backups.pop()
+            if backup_time <= dt_incr - current_delta:
+                dt_incr = backup_time
+                to_keep.append(name)
+                num_backups_in_generation -= 1
+
+    return to_keep
+
---
+++ src/tarsnapper/script.py
@@ -0,0 +1,146 @@
+import sys
+import argparse
+import subprocess
+import re
+from datetime import datetime, timedelta
+
+import expire
+
+
+class CommandError(Exception):
+    pass
+
+
+def call_tarsnap(arguments, options):
+    """
+    ``arguments`` is a single list of strings, ``options`` is a list of
+    key value pairs.
+    """
+    call_with = ['tarsnap']
+    call_with.extend(arguments)
+    for key, value in options:
+        call_with.extend(["--%s" % key, value])
+    print call_with
+    p = subprocess.Popen(call_with, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    p.wait()
+    if p.returncode != 0:
+        raise RuntimeError('tarsnap execution failed: %s', p.stderr.read())
+    return p.stdout
+
+
+DATE_FORMATS = (
+    '%Y-%m-%dT%H:%M:%S.Z',
+    '%Y%m%d-%H%M'
+)
+
+def parse_date(string, dateformat=None):
+    """Parse a date string using either a list of builtin formats,
+    or the given one.
+    """
+    for to_try in ([dateformat] if dateformat else DATE_FORMATS):
+        try:
+            return datetime.strptime(string, to_try)
+        except ValueError:
+            pass
+    else:
+        raise ValueError('"%s" is not a supported date format' % string)
+
+
+def tarsnap_expire(deltas, regex, dateformat, options):
+    """Call into tarsnap, parse the list of archives, then proceed to
+    actually have tarsnap delete those archives we need to expire
+    according to the deltas defined.
+    """
+    regex = re.compile(regex)
+
+    # Build a list of existing backups
+    response = call_tarsnap(['--list-archives'], options)
+    backups = {}
+    for backup_path in response.readlines():
+        match = regex.match(backup_path)
+        if not match:
+            continue
+        date = parse_date(match.groupdict()['date'], dateformat)
+        backups[match.group(0)] = date
+
+    # Determine which backups we need to get rid of, which to keep
+    print backups
+    to_keep = expire.expire(backups, deltas)
+    print to_keep
+
+    # Delete all others
+    for name, _ in backups.items():
+        if not name in to_keep:
+            print "deleting", name
+            call_tarsnap(['-d', '-f', name], options)
+        else:
+            print "keeping", name
+
+
+def timedelta_string(value):
+    """Parse a string to a timedelta value.
+    """
+    if value.endswith('s'):
+        return timedelta(seconds=int(value[:-1]))
+    elif value.endswith('h'):
+        return timedelta(seconds=int(value[:-1]) * 3600)
+    elif value.endswith('d'):
+        return timedelta(days=int(value[:-1]))
+    raise argparse.ArgumentTypeError('invalid delta value: %r (suffix d, s allowed)' % value)
+
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Make backups.')
+    parser.add_argument('--expire', action='store_true',
+                        help='expire only, don\'t make backups')
+    parser.add_argument('--config', '-c', help='use the given config file')
+    parser.add_argument('--regex', help='regex to use to parse the date ' +
+                        'from a backup filename.')
+    parser.add_argument('--deltas', '-d', metavar='DELTA',
+                        type=timedelta_string,
+                        help='generation deltas', nargs='+')
+    parser.add_argument('--dateformat', '-f', help='dateformat')
+    parser.add_argument('-o', metavar=('name', 'value'), nargs=2,
+                        dest='tarsnap_options', action='append', default=[],
+                        help='option to pass to tarsnap')
+    parser.add_argument('jobs', metavar='job', nargs='*')
+    args = parser.parse_args(argv)
+
+    # Do some argument validation that would be to much to ask for
+    # argparse to handle internally.
+    if not args.expire:
+        raise CommandError('--expire is required, for now')
+    if args.config:
+        raise CommandError('--config is not yet supported')
+    if args.config and (args.regex or args.dateformat or args.deltas):
+        raise CommandError('If --config is used, then --regex, --deltas and '
+                           '--dateformat are not available')
+    if args.jobs and not args.config:
+        raise CommandError(('Specific jobs (%s) can only be given if a '
+                            'config file is used') % args.jobs)
+    if not args.config and not args.deltas or not args.regex:
+        raise CommandError('If no config file is used, both --regex and '
+                           '--deltas need to be given')
+
+    jobs = []
+    if args.config:
+        pass  # XXX
+    else:
+        # Only a single job, as given on the command line
+        jobs.append({'regex': args.regex, 'dateformat': args.dateformat,
+                     'deltas': args.deltas})
+
+    for job in jobs:
+        # Do a new backup
+        if not args.expire:
+            pass # XXX
+
+        # Delete old backups
+        tarsnap_expire(job['deltas'], job['regex'], job['dateformat'],
+                       args.tarsnap_options)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]) or 0)

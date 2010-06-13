7b54e7fc5bb0eefdb5612f60d688501f2463c2a6
Michael Elsdoerfer <michael@elsdoerfer.com>
2010-06-13 18:00:36 +0200

    New expire algorithm. Hopefully, this works better.
    There should be tests.

--- src/tarsnapper/expire.py
+++ src/tarsnapper/expire.py
@@ -1,3 +1,4 @@
+import operator
 from datetime import datetime, timedelta
 
 
@@ -16,28 +17,27 @@ def expire(backups, deltas):
     """Given a dict of backup name => backup timestamp pairs in
     ``backups``, and a list of ``timedelta`` objects in ``deltas`` defining
     the generations, will decide which of the backups can be deleted using
-    a  grandfather-father-son backup strategy.
+    a grandfather-father-son backup strategy.
 
-    This approach insists on keeping a minimum number of backups in each
-    generation, regardless of their timestamp. The number of backups in a
-    generation is implicitly defined by the deltas. For example, if the
-    first delta is 1 hour, and the second delta is 1 day, then we will
-    always try to keep 24 backups in the first generation, even if they
-    stretch over, say, the timespan of a year. So for example, if you put
-    your backups on hold for a year, and then pick up again, the old backups
-    will not be immediately deleted, but only after a sufficient number of
-    new backups are available.
+    The approach chosen tries to achieve the following:
 
-    This is what I wanted to accomplish:
+    * Do not require backup names to include information on which generation
+      a backup belongs to, like for example ``tarsnap-generations`` does.
+      That is, you can create your backups anyway you wish, and simply use
+      this utility to delete old backups.
 
-      * Do not require backup names to include information on which
-        generation a backup belongs to, like for example
-        ``tarsnap-generations`` does.
+    * Do not use any fixed generations (weekly, monthly etc), but freeform
+      timespans.
 
-      * Do not make any assumptions about when the backup jobs have
-        actually run (this expire function is completely separate from the
-        actual backup creation), and when in doubt opt to keep backups
-        rather than deleting them.
+    * Similarily, do not make any assumptions about when or if backup jobs
+      have actually run or will run, but try to match the given deltas as
+      closely as possible.
+
+    What the code actually does is, for each generation, start at a fixed
+    point in time determined by the most recent backup (which is always
+    kept) plus the parent generation's delta and then repeatedly stepping
+    the generation's delta forwards in time, chosing a backup that fits
+    best which will then be kept.
 
     Returned is a list of backup names.
     """
@@ -49,33 +49,48 @@ def expire(backups, deltas):
 
     # First, sort the backups with most recent one first
     backups = [(name, time) for name, time in backups.items()]
-    backups.sort(cmp=lambda x, y: cmp(x[1], y[1]))
+    backups.sort(cmp=lambda x, y: -cmp(x[1], y[1]))
     old_backups = backups[:]
 
     # Also make sure that we have the deltas in ascending order
     deltas = list(deltas[:])
     deltas.sort()
 
-    latest_backup = backups.pop()
-    dt_incr = latest_backup[1]
-    to_keep = [latest_backup[0]]
-    while len(deltas) > 1:   # the last delta is the final barrier
-        current_delta = deltas.pop(0)
-        next_delta = deltas[0]
-        num_backups_in_generation = timedelta_div(next_delta, current_delta)
+    # Always keep the most recent backup
+    most_recent_backup = backups[0][1]
+    to_keep = set([backups[0][0]])
 
-        # For the number of backups needed, use the first backup that is
-        # not younger than the requested limit.
-        # XXX: Actually, this is pretty broken. No backup would ever
-        # graduate to an older generation, and backups already in older
-        # generations would never expire. If anything, we would need to
-        # use the first backup that is younger than the limit.
-        while num_backups_in_generation > 0 and backups:
-            name, backup_time = backups.pop()
-            if backup_time <= dt_incr - current_delta:
-                dt_incr = backup_time
-                to_keep.append(name)
-                num_backups_in_generation -= 1
+    # Then, for each delta/generation, determine which backup to keep
+    last_delta = deltas.pop()
+    while deltas:
+        current_delta = deltas.pop()
 
-    return to_keep
+        # (1) Start from the point in time where the current generation ends.
+        dt_pointer = most_recent_backup - last_delta
+        last_selected = None
+        while dt_pointer < most_recent_backup:
+            # (2) Find the backup that matches the current position best.
+            # We have different options here: Take the closest older backup,
+            # take the closest newer backup, or just take the closest backup
+            # in general. We do the latter. The difference is merely in how
+            # long the oldest backup in each generation should be kept, that
+            # is, how the given deltas should be interpreted.
+            by_dist = sorted([(bn, bd, abs(bd - dt_pointer)) for bn, bd in backups], key=operator.itemgetter(2))
+            if by_dist:
+                if by_dist[0][0] == last_selected:
+                    # Not enough backups, don't loop endlessly. This would
+                    # happen in some cases where you'd have a delta much
+                    # smaller than the time difference between some backups.
+                    break
+                last_selected = by_dist[0][0]
+                to_keep.add(by_dist[0][0])
+                # (3) Proceed forward in time, jumping by the current
+                # generation's delta.
+                dt_pointer = by_dist[0][1] + current_delta
+            else:
+                # No more backups found in this generation.
+                break
 
+        last_delta = current_delta
+
+    return list(to_keep)

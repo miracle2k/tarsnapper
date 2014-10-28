ecab63edb8c8819efb829070f668df567874bbc7
Michael Elsdörfer <michael@elsdoerfer.com>
2014-10-28 14:25:45 +0330

    simulate script can now run w/ custom values.

--- simulate.py
+++ simulate.py
@@ -11,15 +11,36 @@ from tarsnapper.config import parse_deltas
 
 
 def main(argv):
-    s = BackupSimulator(parse_deltas('1d 7d 30d'))
+    if '-h' in argv:
+        print "./simulate.py [backup-timestamps]"
+        return 1
 
-    until = s.now + timedelta(days=17)
-    while s.now <= until:
-        s.go_by(timedelta(days=1))
-        s.backup()
+    # Do some default simulation
+    if not argv:
+        s = BackupSimulator(parse_deltas('1d 7d 30d'))
 
-    for name, date in s.backups.iteritems():
-        print name
+        until = s.now + timedelta(days=17)
+        while s.now <= until:
+            s.go_by(timedelta(days=1))
+            s.backup()
+
+        for name, date in s.backups.iteritems():
+            print name
+
+    # Simulate a backup with the timestamps given
+    else:
+        s = BackupSimulator(parse_deltas('1d 7d 30d'))
+        s.add([d for d in argv])
+        deleted, _ = s.expire()
+
+        print "Deleted backups:"
+        for name in deleted:
+            print(name)
+
+        print ""
+        print "Remaining backups:"
+        for name, date in s.backups.iteritems():
+            print name
 
 
 if __name__ == '__main__':
